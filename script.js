f39d9de7068382ca90417bac66ee614da1f1d001
ashraf <bakr29420@gmail.com>
2024-11-22 21:05:30 +0200

    Create Product and store it in localStorage

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 "use strict"
 let GEBID = (id) => document.getElementById(id) ;
 // getTotal()
-// updateProduct()
-// deleteProduct()
-// storeLocal()
-// getLocal()
-// clearInputs()
-// update()
-// search()
-// cleanData()
-
 let title = GEBID('title');
 let price = GEBID('price');
 let taxes = GEBID('taxes');
@@ -31,3 +22,45 @@ function getTotal() {
     }
 
 };
+
+
+// CcreateProduct
+
+let dataPro = JSON.parse(localStorage.product) || []    ;
+
+submit.onclick = () => {
+    let newPro = {
+        title: title.value ,
+        price: price.value ,
+        taxes: taxes.value ,
+        ads: ads.value ,
+        discount: discount.value ,
+        total: total.innerHTML ,
+        count: count.value,
+        category: category.value
+    }
+    dataPro.push(newPro);
+    localStorage.setItem('product' , JSON.stringify(dataPro));
+    console.log(dataPro);
+    clearInputs();
+}
+
+// updateProduct()
+// deleteProduct()
+// storeLocal()
+// getLocal()
+// clearInputs()
+
+let clearInputs = () => {
+    title.value = '' ;
+    price.value = '' ;
+    taxes.value = '' ;
+    ads.value = '' ;
+    discount.value = '' ;
+    category.value = '';
+    count.value = 1 ;
+}
+
+// update()
+// search()
+// cleanData()
